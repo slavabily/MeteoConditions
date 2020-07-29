1ccb12cc2f9d4bb39ec1f460b865fe083b8fc84d
slavabily <slavabily@me.com>
2020-07-29 15:06:17 +0300

    Initial Commit

---
+++ MeteoConditions/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MeteoConditions
+//
+//  Created by slava bily on 29/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
